35faf5203e39a3975a807d053540fb7a7cf12a2c
yubron <wlstn4966@gmail.com>
2020-10-19 21:10:33 +0900

    security config

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const {User} = require("./models/User");
 
-const mongoose = require("mongoose")
-mongoose.connect('mongodb+srv://yubron:happylif2@inflearn.fsyhy.mongodb.net/inflearn?retryWrites=true&w=majority', {
+// applicaion/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+// applicaion/json
+app.use(bodyParser.json());
+
+const config = require('./config/key');
+
+const mongoose = require('mongoose')
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex: true, useFindAndModify:false
 }).then(() => console.log('MongoDB Connected !'))
 .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World testetsteasasasas!')
+})
+
+app.post('/register', (req, res) => {
+    // insert data when register from client
+    
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if(err) return res.json({success : false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
